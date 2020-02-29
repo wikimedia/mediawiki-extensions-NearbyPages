fbc169c66cca232d74908e5fbb49937a27a536bd
Ammar Abdulhamid <ammarpad@yahoo.com>
2020-02-29 15:08:25 +0100

    NearbyPages: Add special page aliases
    Bug: T246532
    Change-Id: Ia86b646db3b43be4da0e96198a0c87cdbce788de

---
+++ NearbyPages.alias.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * Aliases for SpecialNearby special page
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Nearby' => [ 'Nearby' ],
+];
+
+/** Afrikaans (Afrikaans) */
+$specialPageAliases['af'] = [
+	'Nearby' => [ 'Naby' ],
+];
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = [
+	'Nearby' => [ 'قريب' ],
+	];
+
+/** Egyptian Arabic (مصرى) */
+$specialPageAliases['arz'] = [
+	'Nearby' => [ 'قريب' ],
+];
+
+/** Asturian (asturianu) */
+$specialPageAliases['ast'] = [
+	'Nearby' => [ 'Cercano' ],
+];
+
+/** Avaric (авар) */
+$specialPageAliases['av'] = [
+	'Nearby' => [ 'Поблизости' ],
+];
+
+/** Western Balochi (بلوچی رخشانی) */
+$specialPageAliases['bgn'] = [
+	'Nearby' => [ 'بئ_نزینکی_ئا' ],
+];
+
+/** Bengali (বাংলা) */
+$specialPageAliases['bn'] = [
+	'Nearby' => [ 'কাছাকাছি' ],
+];
+
+/** Catalan (català) */
+$specialPageAliases['ca'] = [
+	'Nearby' => [ 'A_prop' ],
+];
+
+/** Chechen (нохчийн) */
+$specialPageAliases['ce'] = [
+	'Nearby' => [ 'Гергахьо' ],
+];
+
+/** Czech (čeština) */
+$specialPageAliases['cs'] = [
+	'Nearby' => [ 'Poblíž' ],
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'Nearby' => [ 'In_der_Nähe' ],
+];
+
+/** Zazaki (Zazaki) */
+$specialPageAliases['diq'] = [
+	'Nearby' => [ 'Nezdıra' ],
+];
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = [
+	'Nearby' => [ 'Apude' ],
+];
+
+/** Spanish (español) */
+$specialPageAliases['es'] = [
+	'Nearby' => [ 'Cerca' ],
+];
+
+/** Estonian (eesti) */
+$specialPageAliases['et'] = [
+	'Nearby' => [ 'Lähikond' ],
+];
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = [
+	'Nearby' => [ 'درنزدیکی', 'در_نزدیکی' ],
+];
+
+/** Galician (galego) */
+$specialPageAliases['gl'] = [
+	'Nearby' => [ 'Preto', 'Cerca' ],
+];
+
+/** Hebrew (עברית) */
+$specialPageAliases['he'] = [
+	'Nearby' => [ 'בסביבה', 'קרוב' ],
+];
+
+/** Hindi (हिन्दी) */
+$specialPageAliases['hi'] = [
+	'Nearby' => [ 'आस-पास' ],
+];
+
+/** Upper Sorbian (hornjoserbsce) */
+$specialPageAliases['hsb'] = [
+	'Nearby' => [ 'W_bliskosći' ],
+];
+
+/** Italian (italiano) */
+$specialPageAliases['it'] = [
+	'Nearby' => [ 'NelleVicinanze' ],
+];
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = [
+	'Nearby' => [ '付近' ],
+];
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = [
+	'Nearby' => [ '근처' ],
+];
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = [
+	'Nearby' => [ 'Nobäi' ],
+];
+
+/** Northern Luri (لۊری شومالی) */
+$specialPageAliases['lrc'] = [
+	'Nearby' => [ 'نئزیک_ڤئ' ],
+];
+
+/** Literary Chinese (文言) */
+$specialPageAliases['lzh'] = [
+	'Nearby' => [ '近郊' ],
+];
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = [
+	'Nearby' => [ 'ВоБлизина' ],
+];
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = [
+	'Nearby' => [ 'സമീപസ്ഥം' ],
+];
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = [
+	'Nearby' => [ 'InDeBuurt' ],
+];
+
+/** Norwegian Nynorsk (norsk nynorsk) */
+$specialPageAliases['nn'] = [
+	'Nearby' => [ 'Nær_her' ],
+];
+
+/** Polish (polski) */
+$specialPageAliases['pl'] = [
+	'History' => [ 'Historia' ],
+	'Nearby' => [ 'W_pobliżu' ],
+];
+
+/** Russian (русский) */
+$specialPageAliases['ru'] = [
+	'Nearby' => [ 'Поблизости' ],
+];
+
+/** Slovak (slovenčina) */
+$specialPageAliases['sk'] = [
+	'Nearby' => [ 'V_okolí', 'VOkolí', 'V_okoli', 'VOkoli' ],
+];
+
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+$specialPageAliases['sr-ec'] = [
+	'Nearby' => [ 'У_близини', 'Близу', 'Оближње' ],
+];
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$specialPageAliases['sr-el'] = [
+	'Nearby' => [ 'U_blizini', 'Blizu', 'Obližnje' ],
+];
+
+/** Swedish (svenska) */
+$specialPageAliases['sv'] = [
+	'Nearby' => [ 'Nära' ],
+];
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = [
+	'Nearby' => [ 'Yakınımdakiler' ],
+];
+
+/** Ukrainian (українська) */
+$specialPageAliases['uk'] = [
+	'Nearby' => [ 'Поблизу', 'Поблизости' ],
+];
+
+/** Urdu (اردو) */
+$specialPageAliases['ur'] = [
+	'Nearby' => [ 'آس_پاس', 'پاس_پڑوس' ],
+];
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = [
+	'Nearby' => [ 'Lân_cận', 'Kề_bên' ],
+];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'Nearby' => [ '附近' ],
+];
+
+/** Simplified Chinese (中文（简体）‎) */
+$specialPageAliases['zh-hans'] = [
+	'Nearby' => [ '附近' ],
+];
+
+/** Traditional Chinese (中文（繁體）‎) */
+$specialPageAliases['zh-hant'] = [
+	'Nearby' => [ '附近' ],
+];
